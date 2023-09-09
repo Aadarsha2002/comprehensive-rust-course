726e35d25c8c313c3c91be7fec8cc42c8d4d03d4
Aadarsha Gopala Reddy <adurs2002@gmail.com>
2023-09-09 11:38:50 -0400

    small-example

---
+++ small-example/target/debug/deps/small_example.d
@@ -0,0 +1,5 @@
+C:\Users\adurs\source\repos\repos\comprehensive-rust-course\small-example\target\debug\deps\small_example.exe: src\main.rs
+
+C:\Users\adurs\source\repos\repos\comprehensive-rust-course\small-example\target\debug\deps\small_example.d: src\main.rs
+
+src\main.rs:
---
+++ small-example/target/debug/small-example.d
@@ -0,0 +1,1 @@
+C:\Users\adurs\source\repos\repos\comprehensive-rust-course\small-example\target\debug\small-example.exe: C:\Users\adurs\source\repos\repos\comprehensive-rust-course\small-example\src\main.rs
