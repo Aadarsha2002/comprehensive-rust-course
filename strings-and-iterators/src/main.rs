e566d5fd25d619c2091db143df4772f135197248
Aadarsha Gopala Reddy <adurs2002@gmail.com>
2023-09-13 21:02:12 -0400

    strings-and-iterators

---
+++ strings-and-iterators/src/main.rs
@@ -0,0 +1,49 @@
+// TODO: remove this when you're done with your implementation.
+#![allow(unused_variables, dead_code)]
+
+#[test]
+fn test_matches_without_wildcard() {
+    assert!(prefix_matches("/v1/publishers", "/v1/publishers"));
+    assert!(prefix_matches("/v1/publishers", "/v1/publishers/abc-123"));
+    assert!(prefix_matches("/v1/publishers", "/v1/publishers/abc/books"));
+
+    assert!(!prefix_matches("/v1/publishers", "/v1"));
+    assert!(!prefix_matches("/v1/publishers", "/v1/publishersBooks"));
+    assert!(!prefix_matches("/v1/publishers", "/v1/parent/publishers"));
+}
+
+#[test]
+fn test_matches_with_wildcard() {
+    assert!(prefix_matches(
+        "/v1/publishers/*/books",
+        "/v1/publishers/foo/books"
+    ));
+    assert!(prefix_matches(
+        "/v1/publishers/*/books",
+        "/v1/publishers/bar/books"
+    ));
+    assert!(prefix_matches(
+        "/v1/publishers/*/books",
+        "/v1/publishers/foo/books/book1"
+    ));
+
+    assert!(!prefix_matches("/v1/publishers/*/books", "/v1/publishers"));
+    assert!(!prefix_matches(
+        "/v1/publishers/*/books",
+        "/v1/publishers/foo/booksByAuthor"
+    ));
+}
+
+pub fn prefix_matches(prefix: &str, request_path: &str) -> bool {
+    let mut request_path = request_path.split('/');
+    for prefix_path in prefix.split('/') {
+        match request_path.next() {
+            Some(request_path) => {
+                if request_path != prefix_path && prefix_path != "*" {
+                    return false;
+                }
+            }
+            None => return false,
+        }
+    }
+}
---
+++ strings-and-iterators/target/debug/deps/strings_and_iterators-f06305d5617b9da6.d
@@ -0,0 +1,5 @@
+C:\Users\adurs\source\repos\repos\comprehensive-rust-course\strings-and-iterators\target\debug\deps\strings_and_iterators-f06305d5617b9da6.exe: src\main.rs
+
+C:\Users\adurs\source\repos\repos\comprehensive-rust-course\strings-and-iterators\target\debug\deps\strings_and_iterators-f06305d5617b9da6.d: src\main.rs
+
+src\main.rs:
